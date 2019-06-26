73f4ce557c1d539eb5666db85588023e1f185348
Liang <liachen@thoughtworks.com>
2019-06-26 14:50:34 +0800

    add WebSocket integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {KafkaProducer} from './kafka-producer';
+import {ApplicationGateway} from './app.gateway';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, KafkaProducer],
+  providers: [AppService, KafkaProducer, ApplicationGateway],
 })
 export class AppModule {}
